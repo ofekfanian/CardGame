02de4ee99c5414144d03b5b15bedcfafd2578c5e
ofekfanian <ofekfanian689@gmail.com>
2026-05-31 19:02:59 +0300

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by ofek fanian on 31/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
